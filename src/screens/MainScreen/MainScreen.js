1e85af5325f63772031765bfc29e64a693ce1a7b
geralb98 <gsecretlb@gmail.com>
2025-05-24 15:03:22 -0600

    refactor:mainscreen components

--- src/screens/MainScreen/MainScreen.js
+++ src/screens/MainScreen/MainScreen.js
@@ -1,44 +1,57 @@
-import { View, TouchableOpacity, StyleSheet, StatusBar, Text, ActivityIndicator } from "react-native";
+import { View, TouchableOpacity, StyleSheet, StatusBar, Text, ActivityIndicator,Image,TextInput } from "react-native";
 import { useState } from "react";
 import { CustomTouchable } from "../../components/CustomTouchable/customTouchable";
 
 export const MainScreen = () => {
 
-    const [isActive, setIsActive] = useState(false);
-    const [counter, setCounter] = useState(0);
-    const [isEven, setIsEven] = useState(false);
-    //const handlerActive = (isActive) = setIsActive(isActive)
-
-    const handlerStates = (isActive) => {
-        setIsActive(isActive);
-        setCounter(counter + 1);
-        const even = counter & 2 ? true : false;
-        console.log("even:", even);
-        console.log("Counter: ", counter);
-        console.log("Status: ", isActive);
-    }
-
-    const handlerActive = () => console.log("Dentro del boton");
+   
+    
     return (
-        <View>
-            {/* <Text>Open up App.js to start working on your app!</Text> */}
-            <StatusBar style="auto" />
-
-            <View>
-                <TouchableOpacity onPress={() => handlerStates(!isActive)}>
-                    <Text>Activar</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View>
-                <Text>
-                    {isActive ? 'Hola mundo' : ''}
-                </Text>
-            </View>
-            <View>
-                <CustomTouchable onPress={() => handlerActive()} title='Presionar'></CustomTouchable>
-                <ActivityIndicator color='orange' size='large'></ActivityIndicator>
-            </View>
+       <View style={styles.container}>
+            <Image style={styles.backgroundimage} source={require('../../../assets/pokemonfondo.jpg')}/>
+            <Image style={styles.backgroundimage2} source={require('../../../assets/pokedex.png')}/>
+            <TextInput
+            placeholder="Ingresa el nombre del Pokemon"
+            style={styles.input}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    backgroundimage: {
+        width: 550,
+        height: 200,
+        position: 'absolute',
+        top: 30,
+        left: 'auto',
+    },
+    backgroundimage2: {
+        width: 300,
+        height: 100,
+        position: 'absolute',
+        top: 80,
+        left: 'auto',
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        padding: 10,
+        paddingStart: 30,
+        width: '80%',
+        height: 50,
+        marginTop: 20,
+        borderRadius: 30,
+        backgroundColor: 'white',
+        position: 'absolute',
+        top: 180,
+        left: 'auto',
+    },
+})
+

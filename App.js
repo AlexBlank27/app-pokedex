0d4011f0793a8d80c984e794e2e09eb644354053
EMTaandrade <aandrade@emt.dev>
2025-05-24 15:48:11 -0600

    feat: get pokemon from api

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { MainScreen } from './src/screens/MainScreen/MainScreen';
+import { MainScreen } from './src/screens/MainScreen/MainScreen';import { GetPokemon } from './src/hooks/FetchPokemon';
 export default function App() {
+
+  async function response() {
+    await GetPokemon();
+  }
+
+  response();
   return (
     <MainScreen></MainScreen>
   );

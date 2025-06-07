051007989c6b9c432e8872fad115e49b9f7f793a
geralb98 <gsecretlb@gmail.com>
2025-06-07 13:24:29 -0600

    feat: splash screen changed

--- App.js
+++ App.js
@@ -2,17 +2,55 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { GetPokemon } from './src/hooks/FetchPokemon';
 import { MainScreen } from './src/screens/MainScreen/MainScreen';
+import * as SplashScreen from 'expo-splash-screen';
+import { useState, useEffect, useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+// SplashScreen.setOptions({
+//   duration: 1000,
+//   fade: true,
+// });
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
 
-  async function response() {
-    await GetPokemon();
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      SplashScreen.hide();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
-
-  response();
   return (
-    <MainScreen></MainScreen>
+    <><View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      onLayout={onLayoutRootView}>
+      <Text>SplashScreen Demo! 👋</Text>
+    </View></>
   );
 }
+
+async function response() {
+  await GetPokemon();
+}
+
+response();
+
 
 const styles = StyleSheet.create({
   container: {
